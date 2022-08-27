91986cee1de468f4110ad39f0f5d3e7f76243a39
namt93 <namt7115633@gmail.com>
2022-08-27 22:05:39 +0700

    add get method of api

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# request Get method url: "/"
+@app.get("/")
+def root():
+    return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
