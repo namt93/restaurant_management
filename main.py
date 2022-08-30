1639709913e831a13613ceab6bcd7aca82a96181
namt93 <namt7115633@gmail.com>
2022-08-31 00:09:42 +0700

    add function C and R of CRUD

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
+
+# global database
+my_posts = [{"title": "title of content 1", "content": "content of post 1", "id": 1},{"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
 # request Get method url: "/"
 @app.get("/")
@@ -10,4 +17,24 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
+
+#@app.post("/createposts")
+#def create_posts(payload: dict = Body(...)):
+#    print(payload)
+#    return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
+
+
+# ultimate a data we expect
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+@app.post("/posts")
+def create_posts(new_post: Post):
+    post_dict = new_post.dict()
+    post_dict['id'] = randrange(0,10000)
+    my_posts.append(post_dict)
+    return {"data":post_dict}
